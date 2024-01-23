b785d935d7fc54d44a9ebe4b4e8db9ce02fa714a
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-01-23 08:38:34 -0700

    Added lots of imports

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -2,6 +2,18 @@ package poke.view;
 
 import poke.controller.Controller;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import javax.swing.SpringLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+import java.awt.Color;
+import java.awt.GridLayout;
 
 public class PokePanel extends JPanel
 {
