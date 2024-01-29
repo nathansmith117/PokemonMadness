13cbe14c99ddd4236f37b4054046eeb372622b09
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-01-29 08:17:28 -0700

    Change image label layout a tiny bit

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -122,8 +122,8 @@ public class PokePanel extends JPanel
 		layout.putConstraint(SpringLayout.SOUTH, fieldPanel, -25, SpringLayout.SOUTH, this);
 		
 		layout.putConstraint(SpringLayout.NORTH, imageLabel, 150, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.WEST, imageLabel, 150, SpringLayout.WEST, this);
-		layout.putConstraint(SpringLayout.EAST, imageLabel, -100, SpringLayout.WEST, fieldPanel);
+		layout.putConstraint(SpringLayout.WEST, imageLabel, 250, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, imageLabel, -50, SpringLayout.WEST, fieldPanel);
 		
 		layout.putConstraint(SpringLayout.WEST, pokedexSelector, 150, SpringLayout.WEST, this);
 		layout.putConstraint(SpringLayout.EAST, pokedexSelector, -150, SpringLayout.WEST, fieldPanel);
