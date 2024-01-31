48cf6a39b7221c5900a53bc34b36f6e887f83e35
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-01-31 08:09:39 -0700

    Worked on loadData lmao

--- src/poke/controller/IOController.java
+++ src/poke/controller/IOController.java
@@ -2,6 +2,7 @@ package poke.controller;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
 
@@ -14,9 +15,25 @@ public class IOController
 {
 	public static ArrayList<Pokemon> loadData(String dataFile, Controller app)
 	{
-		ArrayList<Pokemon> savedPokeList = null;
+		ArrayList<Pokemon> savedPokemonList = null;
 		
-		return savedPokeList;
+		try (FileInputStream loadStream = new FileInputStream(dataFile);
+				ObjectInputStream input = new ObjectInputStream(loadStream))
+		{
+			ArrayList<Pokemon> loadedPokemon = new ArrayList<Pokemon>();
+			loadedPokemon = (ArrayList<Pokemon>)input.readObject();
+			savedPokemonList = loadedPokemon;
+		}
+		catch (IOException readError)
+		{
+			JOptionPane.showMessageDialog(app.getWindow(), readError.getMessage(), "Today isn't very smurf! Funny little file loading error lol", JOptionPane.ERROR_MESSAGE);
+		}
+		catch (ClassNotFoundException classError)
+		{
+			JOptionPane.showMessageDialog(app.getWindow(), classError.getMessage(), "Its a smurfing class error ):", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		return savedPokemonList;
 	}
 	
 	public static void saveData(String dataFile, ArrayList<Pokemon> pokemonList, Controller app)
