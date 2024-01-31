d2a8a3b048d564b235634cdb5f346dc5ac0aac2a
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-01-31 07:59:18 -0700

    More IOController things

--- src/poke/controller/IOController.java
+++ src/poke/controller/IOController.java
@@ -2,7 +2,7 @@ package poke.controller;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.IOException;
 
 import java.util.ArrayList;
@@ -12,5 +12,23 @@ import poke.model.monsters.Pokemon;
 
 public class IOController
 {
+	public static ArrayList<Pokemon> loadData(String dataFile, Controller app)
+	{
+		ArrayList<Pokemon> savedPokeList = null;
+		
+		return savedPokeList;
+	}
 	
+	public static void saveData(String dataFile, ArrayList<Pokemon> pokemonList, Controller app)
+	{
+		try (FileOutputStream saveStream = new FileOutputStream(dataFile);
+				ObjectOutputStream output = new ObjectOutputStream(saveStream))
+		{
+			output.writeObject(pokemonList);
+		}
+		catch (IOException saveError)
+		{
+			JOptionPane.showMessageDialog(app.getWindow(), saveError.getMessage(), "OH SMURF! Coudn't save this mf", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 }
